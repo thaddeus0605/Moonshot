db4102a17f41023cd122f7a178491a2e117ec3a5
thaddeus0605 <tdronski@gmail.com>
2022-12-22 12:45:22 -0600

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Thaddeus Dronski on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
